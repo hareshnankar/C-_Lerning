5e435bf0180a1e2bd9782e2a52e87fde83e2f9a7
hareshnankar <hareshnankar1234@GMAIL.COM>
2025-07-05 10:55:34 +0530

    structural changes

--- Make_file_concept/basics_of_make/basics_tutorial_2/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include "utils.h"
-#include <stdio.h>  // Standard I/O functions
-
-void greet();  // Function declaration
-
-#endif
--- Make_file_concept/basics_of_make/basics_tutorial_2/utils.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void print_message() {
-    printf("Hello, Makefile with multiple files!\n");
-}
